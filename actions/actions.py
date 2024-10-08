ff800f4afd39867e250a292c2262623cb390d106
PamelaBorelli <102415224+PamelaBorelli@users.noreply.github.com>
2024-10-08 14:32:44 -0300

    atualizacao

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,62 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
+
+class ActionPedirNumeroPedido(Action):
+
+    def name(self) -> Text:
+        return "action_pedir_numero_pedido"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        numero_pedido = tracker.get_slot('pedido')
+
+        if numero_pedido:
+            dispatcher.utter_message(text=f"Você forneceu o número {numero_pedido}. Processando o seu pedido...")
+            return []
+        else:
+            dispatcher.utter_message(text="Por favor, forneça o número do seu pedido.")
+            return [SlotSet("pedido", None)]
+
+
+class ActionRastreamentoPedido(Action):
+
+    def name(self) -> Text:
+        return "action_rastreamento_pedido"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        numero_pedido = tracker.get_slot('pedido')
+        if numero_pedido:
+            # Simular o rastreamento do pedido
+            dispatcher.utter_message(text=f"O rastreamento para o pedido {numero_pedido} está em andamento.")
+        else:
+            dispatcher.utter_message(text="Ainda não recebi o número do seu pedido.")
+        return [SlotSet("pedido", numero_pedido)]
+
+
+class ActionDevolucao(Action):
+
+    def name(self) -> Text:
+        return "action_devolucao"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        numero_pedido = tracker.get_slot('pedido')
+        if numero_pedido:
+            # Simular o início da devolução
+            dispatcher.utter_message(text=f"A devolução para o pedido {numero_pedido} foi iniciada.")
+        else:
+            dispatcher.utter_message(text="Ainda não recebi o número do seu pedido.")
+        return [SlotSet("pedido", numero_pedido)]
+
